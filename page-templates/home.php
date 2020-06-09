c2743c34bb6764e41cc0baeaf7fd6e85bdf5e882
Nina Presotto <contact@ninapresotto.com>
2020-06-09 10:08:22 +0200

    addhed video header

--- page-templates/home.php
+++ page-templates/home.php
@@ -9,35 +9,50 @@ get_header();
 ?>
 <div class="home-content">
 
-	<div id="first-section" class="section">
+	<?php
+		$titre = get_field('title_home_first_section');
+		$paragraphe = get_field('p_home_first_section');
+		$texte_btn = get_field('texte_bouton_home_first_section');
+		$url = get_field('lien_bouton_home_first_section');
+	?>
+	<div id="first-section" class="text-img-block section limited">
 		<div class="container-fluid">
-			<div class="row">
-				<div class="col-6">
-					<h2>Consectur adipiscing elit sed do eiusmod</h2>
-					<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-					</p>
-					<div class="read-more">
-					<a href="">Lorem ipsum dolor sit amet</a>
+				<div class="row">
+					<div class="col-6 column">
+						<?php if(!empty($titre)): ?>
+						<h3>
+							<?php echo $titre; ?>
+						</h3>
+						<?php endif; ?>
+						<?php if(!empty($paragraphe)): ?>
+						<p>
+						<?php echo $paragraphe; ?>
+						</p>
+						<?php endif; ?>
+						<?php if(!empty($texte_btn) && !empty($url)): ?>
+						<div class="read-more">
+						<a href="<?php echo $url;  ?>"><?php echo $texte_btn; ?></a>
+						</div>
+						<?php endif; ?>
+					</div>
+					<div class="col-6 text-center position-relative column">
+							<img src="<?php echo get_template_directory_uri().'/img/illustr.png'; ?>" class="position-absolute">
 					</div>
 				</div>
-				<div class="col-6 text-center position-relative">
-					<div class="bg">
-						<img src="<?php echo get_template_directory_uri().'/img/illustr.png'; ?>" class="position-absolute">
-					</div>
-				</div>
-			</div>
 		</div>
 	</div>
 
+	<?php
+		$titre = get_field('title_home_testimonials');
+	?>
 	<div id="slider-section">
 		<div class="flex">
 			<div class="testimonial-slider-container">
 				<?php if(have_rows('testimonial_slider')): ?>
-
+	
 				<div class="testimonial-slider">
-
-
+	
+	
 				<?php while(have_rows('testimonial_slider')): the_row(); ?>
 						<?php 
 						$img_profil = get_sub_field('image_profil'); 
@@ -45,7 +60,7 @@ get_header();
 						$review = get_sub_field('review');
 						?>
 						<div style="padding-bottom:1.5em;">
-
+	
 							<div class="testimonial-container">
 						
 								<?php if( !empty( $img_profil ) ): ?>    
@@ -53,53 +68,73 @@ get_header();
 								    	<img class="noresp circle-image" src="<?php echo esc_url($img_profil['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($img_profil['alt']); ?>" />
 									</div>
 								<?php endif; ?>	
-
+	
 								<?php if( !empty( $full_name ) ): ?>    
 									<div class="testimonial-fullname"><?php echo $full_name; ?></div>
 								<?php endif; ?>	
-
+	
 								<?php if( !empty( $review ) ): ?>    
 									<p class="testimonial-review"><?php echo $review; ?></p>
 								<?php endif; ?>	
-
+	
 							</div>
-
+	
 						</div>
-
+	
 					<?php endwhile; ?>
-
+	
 					
 				</div>
 				<?php endif; ?>
 			</div>
-			<div class="left-2"><h2>Testimonials</h2></div>
+			<div class="left-2">
+				<?php if(!empty($titre)): ?>
+				<h3>
+					<?php echo $titre; ?>
+				</h3>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
-
-	<div id="logo-section">
+	
+	<?php
+		$titre = get_field('title_home_logos_clients');
+		$paragraphe = get_field('p_home_logos_clients');
+	?>
+	<div id="logo-section" class="section limited">
 		<div class="container-fluid">
-			<div class="row">
-				<div class="col-5 offset-1 column">
-					<div>
-						<?php if(have_rows('logos_clients')): ?>
-							<ul>
-							<?php while(have_rows('logos_clients')):the_row(); 
-								$img_logo = get_sub_field('image_logo'); ?>
-								<?php if( !empty( $img_logo ) ): ?>    
-									<li>
-								    	<img src="<?php echo esc_url($img_logo['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($img_profil['alt']); ?>" />
-									</li>
-								<?php endif; ?>	
-							<?php endwhile; ?>
-							</ul>
-						<?php endif; ?>
+			<div class="section-container">
+				<div class="row">
+					<div class="col-6 column">
+						<div>
+							<?php if(have_rows('logos_clients')): ?>
+								<ul>
+								<?php while(have_rows('logos_clients')):the_row(); 
+									$img_logo = get_sub_field('image_logo'); ?>
+									<?php if( !empty( $img_logo ) ): ?>    
+										<li>
+									    	<img src="<?php echo esc_url($img_logo['sizes']['thumbnail']); ?>" alt="<?php echo esc_attr($img_profil['alt']); ?>" />
+										</li>
+									<?php endif; ?>	
+								<?php endwhile; ?>
+								</ul>
+							<?php endif; ?>
+						</div>
 					</div>
-				</div>
-				<div class="col-5 position-relative column">
-					<div>
-						<h2>Nos clients</h2>
-						<p>Lorem ipsum dolor est...</p>
-						<div class="arrow"></div>
+					<div class="col-6 position-relative column">
+						<div>
+							<?php if(!empty($titre)): ?>
+							<h3>
+								<?php echo $titre; ?>
+							</h3>
+							<?php endif; ?>
+							<?php if(!empty($paragraphe)): ?>
+							<p>
+								<?php echo $paragraphe; ?>
+							</p>
+							<?php endif; ?>
+							<div class="arrow"></div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -112,7 +147,7 @@ get_header();
 			</div>
 			<div></div>
 		</div>
-	</div>
+	</div> 
 	
 
 </div>
