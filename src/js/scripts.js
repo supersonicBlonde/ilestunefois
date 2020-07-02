b8f8367c5dfed8c4fd40585fa96a5f1c46751b20
Nina Presotto <contact@ninapresotto.com>
2020-07-02 17:14:43 +0200

    regorganised-code-modal

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,92 @@
+class Modal {
+
+	  constructor(parent) {
+	    this.parent = document.getElementById('header-container');
+	    //this.parent = parent;
+	    this.modal_container = document.createElement('div');
+	  	this.close_btn = document.createElement('div');
+	  	this.close_shape = document.createElement('div');
+	    this.createElements();
+	   
+	  }
+
+
+	close() {
+	  let parent = this.parentElement;
+	 document.getElementById("form-connect").classList.add('hide');
+	  let close_btn = this;
+	  this.children[0].style.display = "none"
+	  this.classList.add('hide');
+	  let anim = parent.animate([
+		  // keyframes
+		  { width: '60%', opacity: 1}, 
+	  	  { width: 0, opacity:0.5 }
+
+		], { 
+		  // timing options
+		  duration: 500,
+		  iterations: 1,
+		  delay: 300,
+		  easing: 'ease-out',
+		  fill: "forwards"
+		});
+	  anim.onfinish = function() {
+	  	parent.remove();
+	  }
+	}
+
+	
+
+	createElements() {
+	  /*	this.modal_container = document.createElement('div');
+	  	this.close_btn = document.createElement('div');
+	  	this.close_shape = document.createElement('div');*/
+	  	
+	  	this.parent.appendChild(this.modal_container);
+	  	this.modal_container.appendChild(this.close_btn);
+	  	this.close_btn.appendChild(this.close_shape);
+
+	  	this.modal_container.classList.add('modal-connect');
+	  	this.modal_container.setAttribute('id', 'modal-container');
+
+	  	this.close_btn.classList.add('close-btn');
+		this.close_btn.setAttribute('id', 'close-btn');
+
+		this.close_shape.classList.add('close-shape');
+	 }
+
+	 animate() {
+	 	this.modal_container.classList.add('animate');
+	 	this.close_btn.classList.add('animate');
+	 	this.close_shape.classList.add('animate');
+	 }
+
+	 load_template() {
+	 	jQuery.ajax({
+            url : ajax_js_obj.ajax_url,
+            type : 'post',
+            data : {
+                action : 'load_form', 
+                submitted_nonce : ajax_js_obj.the_nonce,
+            },
+            success : function( response ) {
+				console.log( "success" );
+				let form = document.createElement('div');
+				form.setAttribute('id' , "form-connect");
+				let parent = document.getElementById('modal-container');
+				parent.appendChild(form);
+				form.innerHTML =  response.data;
+            },
+            error : function( response ) {
+                console.log('Error retrieving the information: ' + response.status + ' ' + response.statusText);
+                console.log( response );
+            }
+        });
+		 }
+
+
+	  }
+
 window.addEventListener("load", function() {
 
 	jQuery('.testimonial-slider').slick({
@@ -55,249 +144,20 @@ window.addEventListener("load", function() {
  
 
 	/************************************************
-	         CLASS
+	         MODAL
 			******************************************/
+
 	let connect_btn = document.querySelector('.menu-btn');
 
 	connect_btn.addEventListener('click' , function(event) {
 
 		event.preventDefault();
-		let modal = addModalElement();
-		//let close_btn = addCloseElement();
-		//let shape = addShapeElement();
-		animateModal(modal);
-		//animateClose(close_btn);
-		//animateShape(shape);
-
-		load_template();
-
+		let modal = new Modal();
+		modal.load_template();
+		modal.animate();
+		modal.close_btn.addEventListener('click', modal.close);
 	}); 
 
 });
 
 
-function load_template() {
-
-	jQuery.ajax({
-            url : ajax_js_obj.ajax_url,
-            type : 'post',
-            data : {
-                action : 'load_form',  // Note that this is part of the add_action() call.
-                submitted_nonce : ajax_js_obj.the_nonce,
-                //post_type : post_type
-            },
-            success : function( response ) {
-				// Display both pieces of returned data in different alert boxes.
-				console.log( "success" );
-				let form = document.createElement('div');
-				console.log(form);
-				let parent = document.getElementById('modal-container');
-				parent.appendChild(form);
-				form.innerHTML =  response.data;
-            },
-            error : function( response ) {
-                console.log('Error retrieving the information: ' + response.status + ' ' + response.statusText);
-                console.log( response );
-            }
-        });
-
-    
-   /* fetch(ajax_js_obj.ajax_url , {
-			method: "POST",
-			body: 'action=load_form&submitted_nonce='+ajax_js_obj.the_nonce
-		}).then(res => res.json())
-			.catch(error => {
-				console.log("error");
-			})
-			.then(response => {
-
-				if (response === 0 || response.status === 'error') {
-					console.log( response );
-					return;
-				}
-				let form = document.createElement('div');
-				console.log(form);
-				let parent = document.getElementById('modal-container');
-				parent.appendChild(form);
-				form.innerHTML =  response.data;
-			});
-*/
-}
-
-
-function addCloseElement () { 
-	
-	let close_btn = document.createElement('div');
-	close_btn.classList.add('close-btn');
-	close_btn.setAttribute('id', 'close-btn');
-	let parent = document.getElementById('modal-container');
-	parent.appendChild(close_btn);
-	animateClose(close_btn);
-	close_btn.addEventListener('click' , function(e) {
-		document.querySelector('.close-shape').animate([
-		  // keyframes
-		  { opacity: 1 }, 
-	  	  { opacity: 0 }
-
-		], { 
-		  // timing options
-		  duration: 300,
-		  iterations: 1,
-		  easing: 'ease-out',
-		  fill: "forwards"
-		});
-		this.animate([
-		  // keyframes
-		  { left: '-100px' }, 
-	  	  { left: 0 }
-
-		], { 
-		  // timing options
-		  duration: 300,
-		  delay: 200,
-		  iterations: 1,
-		  easing: 'ease-out',
-		  fill: "forwards"
-		});
-		let animation = parent.animate([
-		  // keyframes
-		  { width: '50vw' }, 
-	  	  { width: 0 }
-
-		], { 
-		  // timing options
-		  duration: 300,
-		  iterations: 1,
-		  delay: 500,
-		  easing: 'ease-out',
-		  fill: "forwards"
-		});
-
-		animation.onfinish = removeModal;
-	})
-	return close_btn;
-	
-}
-function removeModal() {
- document.getElementById("modal-container").remove();
-}
-
-
-function addShapeElement() { 
-	let button = document.getElementById('close-btn');
-	let myshape = document.createElement('div');
-	myshape.classList.add('close-shape');
-	//myshape.innerHTML= '<i class="fi flaticon-close"></i>';
-	button.append(myshape);
-
-	return myshape;
-	
-}
-
-
-function addModalElement() { 
-	let modal = document.createElement('div');
-	modal.setAttribute('id', 'modal-container');
-	modal.classList.add('modal-connect');
-	let parent = document.getElementById('header-container');
-	
-	parent.appendChild(modal);
-
-	return modal;
-	
-}
-
-function animateModal(modal) {
-	let animationModal = modal.animate([
-	  // keyframes
-	  { width: 0}, 
-  	  { width: '60%' }
-
-	], { 
-	  // timing options
-	  duration: 1000,
-	  iterations: 1,
-	  easing: 'ease-out',
-	  fill: "forwards"
-	});
-	
-	//animationModal.onfinish = addCloseElement;
-} 
-
-
-function animateClose(element){
-let animation = element.animate([
-	  // keyframes
-	  { left: 0 }, 
-  	  { left: '-100px' }
-
-	], { 
-	  // timing options
-	  duration: 1000,
-	  iterations: 1,
-	  delay: 1000,
-	  easing: 'ease-out',
-	  fill: "forwards"
-	});
-
-}
-
-
-function animateShape(element){
-	
-	let animation = element.animate([
-	  // keyframes
-	  { opacity: 0 }, 
-  	  { opacity: 1 }
-
-	], { 
-	  // timing options
-	  duration: 1000,
-	  iterations: 1,
-	  delay: 2000,
-	  easing: 'ease-out',
-	  fill: "forwards"
-	});
-
-}
-
-let header_main = document.querySelector('header-container');
-
-if(!document.getElementById('modal-container')) console.log('not exists');
-
-let last_known_scroll_position = 0;
-let ticking = false;
-
-function doSomething(scroll_pos) {
-	  //console.log(scroll_pos);
-	  if(scroll_pos > 200) {
-	  	console.log('bas');
-	  	if(document.getElementById('modal-container')) {
-		   if(document.getElementById('modal-container')) {
-		      	document.querySelector('.modal-connect').classList.remove('show');
-		      	document.querySelector('.modal-connect').classList.add('hide');
-
-		  }
-	  	}
-	  } else if (scroll_pos < 50)  {
-	  	console.log('haut');
-	  	if(document.getElementById('modal-container')) {
-	      	document.querySelector('.modal-connect').classList.add('show');
-	      	document.querySelector('.modal-connect').classList.remove('hide');
-
-	  }
-	}
-}
-
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollY;
-
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
-
-    ticking = true;
-  }
-});
